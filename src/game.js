62516cd881c387f7be048135b60f73f08002c14a
squibel <samquibel@icloud.com>
2025-02-04 00:48:21 +0100

    map end

--- src/game.js
+++ src/game.js
@@ -9,13 +9,13 @@ import TileMap from './TileMap.js';
 const Countbonus = 6;
 const bonus = ['Bonus1', 'Bonus2', 'Bonus3'];
 const map = [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1, 1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1, 1, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 1, 1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1, 1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1, 1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1, 1, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 1, 1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1, 1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-const totalBlockBreakable = countBlock();
+const totalBlockBreakable = countBlockBreakable();
 console.log(totalBlockBreakable);
 const tileMap = new TileMap(map, Countbonus, bonus, totalBlockBreakable);
 
 tileMap.draw();
 
-function countBlock() {
+function countBlockBreakable() {
   let count = 0;
   for (let i = 0; i < map.length; i++) {
     if (map[i] === 5) {
