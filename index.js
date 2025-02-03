18397a34af1c38c3e333dc39ecda752a0063f390
mtliche <tlichemohamed76@gmail.com>
2025-02-03 19:24:37 +0100

    mettre a jour grille dans bomb class

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import {Bomb} from "./src/bomb.js";
-
+//manque la grille dans la bomb element
 export const menu = () => {
     let flame = 1
     window.addEventListener("click", (e) => {
         const bomb = new Bomb(e.clientX, e.clientY, flame)
         bomb.dropBomb()
-        flame++
+        if (flame > 12) {
+
+        } else {
+            flame++
+        }
     })
 }
 
