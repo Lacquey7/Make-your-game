e1c2e3f6efde13061367bd96a6478efe1b306d71
squibel <samquibel@icloud.com>
2025-02-04 13:59:37 +0100

    mapp

--- src/collision.js
+++ src/collision.js
@@ -1,8 +1,5 @@
 export default class Collision {
-    static checkCollision(object1, object2) {
-        return (object1.x < object2.x + 32 &&
-            object1.x + 32 > object2.x &&
-            object1.y < object2.y + 32 &&
-            object1.y + 32 > object2.y);
-    }
+  static checkCollision(object1, object2) {
+    return object1.x < object2.x + 32 && object1.x + 32 > object2.x && object1.y < object2.y + 32 && object1.y + 32 > object2.y;
+  }
 }
--- src/game.js
+++ src/game.js
@@ -4,86 +4,88 @@ import Bot from './bot.js';
 import Collision from './collision.js';
 
 export default class Game {
-    constructor() {
-        this.init();
-    }
+  constructor() {
+    this.init();
+  }
 
-    init() {
-        // Map configuration
-        this.Countbonus = 6;
-        this.bonus = ['Bonus1', 'Bonus2', 'Bonus3'];
-        this.map = [
-            1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
-            1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1,
-            1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1,
-            1, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 1,
-            1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1,
-            1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1,
-            1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1,
-            1, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 1,
-            1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1,
-            1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1,
-            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
-        ];
+  init() {
+    // Map configuration
+    this.Countbonus = 6;
+    this.bonus = ['Bonus1', 'Bonus2', 'Bonus3'];
+    this.map = [
+      [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+      [1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1],
+      [1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1],
+      [1, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 1],
+      [1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1],
+      [1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1],
+      [1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1],
+      [1, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 1],
+      [1, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 1],
+      [1, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 1],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
 
-        this.totalBlockBreakable = this.countBlockBreakable();
-        this.tileMap = new TileMap(this.map, this.Countbonus, this.bonus, this.totalBlockBreakable);
+    this.totalBlockBreakable = this.countBlockBreakable();
+    this.tileMap = new TileMap(this.map, this.Countbonus, this.bonus, this.totalBlockBreakable);
 
-        // Initialize map
-        this.tileMap.draw();
+    // Initialize map
+    this.tileMap.draw();
 
-        // Initialize game objects
-        this.player = new Player();
-        this.bot = new Bot();
+    // Initialize game objects
+    this.player = new Player();
+    this.bot = new Bot();
 
-        // Set up controls
-        this.keys = {
-            ArrowUp: false,
-            ArrowDown: false,
-            ArrowLeft: false,
-            ArrowRight: false
-        };
+    // Set up controls
+    this.keys = {
+      ArrowUp: false,
+      ArrowDown: false,
+      ArrowLeft: false,
+      ArrowRight: false,
+    };
 
-        this.setupEventListeners();
-        this.startGameLoop();
-    }
+    this.setupEventListeners();
+    this.startGameLoop();
+  }
 
-    setupEventListeners() {
-        document.addEventListener('keydown', (e) => {
-            if (this.keys.hasOwnProperty(e.code)) {
-                this.keys[e.code] = true;
-            }
-        });
+  setupEventListeners() {
+    document.addEventListener('keydown', (e) => {
+      if (this.keys.hasOwnProperty(e.code)) {
+        this.keys[e.code] = true;
+      }
+    });
 
-        document.addEventListener('keyup', (e) => {
-            if (this.keys.hasOwnProperty(e.code)) {
-                this.keys[e.code] = false;
-            }
-        });
-    }
+    document.addEventListener('keyup', (e) => {
+      if (this.keys.hasOwnProperty(e.code)) {
+        this.keys[e.code] = false;
+      }
+    });
+  }
 
-    countBlockBreakable() {
-        return this.map.filter(tile => tile === 5).length;
-    }
+  countBlockBreakable() {
+    return this.map.reduce((count, row) => {
+      return count + row.filter((cell) => cell === 5).length;
+    }, 0);
+  }
 
-    startGameLoop() {
-        const gameLoop = () => {
-            this.player.move(this.keys);
-            this.bot.moveAutonomously();
+  startGameLoop() {
+    const gameLoop = () => {
+      this.player.move(this.keys);
+      this.bot.moveAutonomously();
 
-            // Check collision
-            if (Collision.checkCollision(this.player, this.bot)) {
-                console.log("Collision detected!");
-            }
+      // Check collision
+      if (Collision.checkCollision(this.player, this.bot)) {
+        console.log('Collision detected!');
+      }
 
-            requestAnimationFrame(gameLoop);
-        };
+      requestAnimationFrame(gameLoop);
+    };
 
-        gameLoop();
-    }
+    gameLoop();
+  }
 }
 
 // Start game when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
-    new Game();
+  new Game();
 });
