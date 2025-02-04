f4c2e8b37a370238cbbfb9a4af91776259020791
MrFruchard <Romain2702@hotmail.fr>
2025-02-04 09:43:08 +0100

    Ajout d'un bot autonome

--- src/game.js
+++ src/game.js
@@ -1,26 +1,32 @@
-let player = new Player();
-let keys = {
-    ArrowUp: false,
-    ArrowDown: false,
-    ArrowLeft: false,
-    ArrowRight: false
-};
+// Attendre que le DOM soit chargé
+document.addEventListener('DOMContentLoaded', () => {
+    const bot = new Bot();
+    let player = new Player();
+    let keys = {
+        ArrowUp: false,
+        ArrowDown: false,
+        ArrowLeft: false,
+        ArrowRight: false
+    };
 
-document.addEventListener('keydown', (e) => {
-    if (keys.hasOwnProperty(e.code)) {
-        keys[e.code] = true;
+    document.addEventListener('keydown', (e) => {
+        if (keys.hasOwnProperty(e.code)) {
+            keys[e.code] = true;
+        }
+    });
+
+    document.addEventListener('keyup', (e) => {
+        if (keys.hasOwnProperty(e.code)) {
+            keys[e.code] = false;
+        }
+    });
+
+    function gameLoop() {
+        player.move(keys);
+        bot.moveAutonomously();
+        requestAnimationFrame(gameLoop);
     }
+
+    // Démarrer le jeu
+    gameLoop();
 });
-
-document.addEventListener('keyup', (e) => {
-    if (keys.hasOwnProperty(e.code)) {
-        keys[e.code] = false;
-    }
-});
-
-function gameLoop() {
-    player.move(keys);
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
